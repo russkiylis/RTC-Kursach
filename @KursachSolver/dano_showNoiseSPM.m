6fff1147fc7e7e71cfe8ec2159ab340df9554b8f
russkiylis <russkiylis@koshy.ru>
2026-03-30 08:13:47 +0300

    комментослоп

--- @KursachSolver/dano_showNoiseSPM.m
+++ @KursachSolver/dano_showNoiseSPM.m
@@ -1,5 +1,12 @@
 function dano_showNoiseSPM(obj)
-%SHOWSIGNAL Вывод СПМ шума из дано
+% DANO_SHOWNOISESPM — строит график спектральной плотности мощности (СПМ) шума.
+%
+% СПМ квазибелого шума — это прямоугольник:
+%   W(ω) = W0 при |ω| < ω_гр, иначе 0.
+%
+% На графике отмечены:
+%   - Граничные частоты ±ω_гр (вертикальные пунктирные линии)
+%   - Уровень W0 (горизонтальная пунктирная линия)
 
 cyclic_freq = obj.cyclic_freq;
 noiseSPM = obj.noise_SPM;
@@ -7,18 +14,25 @@ W0 = obj.W0;
 omega_gr_n = obj.omega_gr_n;
 
 figure(name="СПМ шума");
+
+% Рисуем СПМ. Добавляем нулевые точки за пределами оси частот,
+% чтобы график корректно уходил в ноль на краях.
 plot([-2.*omega_gr_n cyclic_freq 2.*omega_gr_n], [0 noiseSPM 0], LineWidth=3);
 grid on;
+
+% Ограничиваем оси с запасом
 xlim([-1.5.*omega_gr_n 1.5.*omega_gr_n]);
 ylim([-0.2.*W0 1.3.*W0]);
 
+% Оси через начало координат
 ax = gca;
 ax.XAxisLocation = 'origin';
 ax.YAxisLocation = 'origin';
 
-xline(-omega_gr_n, '--', "-\omegaгр");
-xline(omega_gr_n, '--', "\omegaгр");
-yline(W0, '--', "W0");
+% Пунктирные линии-маркеры для наглядности
+xline(-omega_gr_n, '--', "-\omegaгр");   % Левая граничная частота
+xline(omega_gr_n, '--', "\omegaгр");      % Правая граничная частота
+yline(W0, '--', "W0");                    % Уровень СПМ
 
 title("СПМ шума, W0 = " + sprintf("%.2e", W0) + " В^2/Гц, |\omegaгр| = " + sprintf("%.2e", omega_gr_n) + " рад/с");
 xlabel("\omega, рад/с");
